7af4cc19fd93165ce59f320095cf5d3c5f7a802f
esentur <esentur19941994@gmail.com>
2026-03-30 15:13:05 +0600

    added hello and variable

--- topics/hello/exercises/src/main.rs
+++ topics/hello/exercises/src/main.rs
@@ -1,7 +1,9 @@
 // Attribute that tells the Rust compiler to suppress warnings
 // about unused code
 // Attributes - metadata for the compiler
-#![allow(unused)]
+fn main() {
+    println!("Hello, Rust!");
+}
 
 // main() is the entry point of Rust program
 
