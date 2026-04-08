53b23bcbd9fae8f4eb561cc3c3ecd4e2454b3a25
esentur19941994 <esentur19941994>
2026-04-09 04:31:20 +0600

    exercise: for_loop

--- topics/for_loop/exercises/src/lib.rs
+++ topics/for_loop/exercises/src/lib.rs
@@ -1,7 +1,18 @@
 pub fn sum(nums: Vec<i32>) -> i32 {
-    todo!();
+    let mut sum = 0;
+
+    for num in nums {
+        sum += num;
+    }
+    sum
+
 }
 
 pub fn fill(i: u32, n: usize) -> Vec<u32> {
-    todo!();
+    let mut result = Vec::new();
+
+    for _ in 0..n {
+        result.push(i);
+    }
+    result
 }
