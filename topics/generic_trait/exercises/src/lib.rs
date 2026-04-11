c11dc07ff99ccebbf321ea438d88efcc00a0e932
esentur19941994 <esentur19941994>
2026-04-12 02:45:40 +0600

    exercise: generic_trait

--- topics/generic_trait/exercises/src/lib.rs
+++ topics/generic_trait/exercises/src/lib.rs
@@ -1,3 +1,4 @@
+
 pub trait Iterator<T> {
     fn next(&mut self) -> Option<&T>;
 }
@@ -12,8 +13,31 @@ pub struct VecIter<T> {
     pub next: usize,
 }
 
+
 impl<T> Iterator<T> for TupleIter<T> {
     fn next(&mut self) -> Option<&T> {
-        todo!();
+        let result = match self.next {
+            0 => Some(&self.tuple.0),
+            1 => Some(&self.tuple.1),
+            2 => Some(&self.tuple.2),
+            _ => None,
+        };
+
+        if result.is_some() {
+            self.next += 1;
+        }
+        result
+    }
+}
+
+
+impl<T> Iterator<T> for VecIter<T> {
+    fn next(&mut self) -> Option<&T> {
+        let result = self.vec.get(self.next);
+
+        if result.is_some() {
+            self.next += 1;
+        }
+        result
     }
 }
